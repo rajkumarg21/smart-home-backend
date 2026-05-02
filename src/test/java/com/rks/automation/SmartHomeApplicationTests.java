310e716e335f3a32f030cca4582aefd3123ad34a
Rajkumar saad <rohitsaad01@gmail.com>
2026-05-02 13:40:04 +0530

    Initial backend setup - Smart Home project

---
+++ src/test/java/com/rks/automation/SmartHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rks.automation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
